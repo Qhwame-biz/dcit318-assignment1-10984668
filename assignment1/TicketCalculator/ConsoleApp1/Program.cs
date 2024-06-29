9838b4bfd970c33bb972b1065b25f0f2021ec264
kwame biz <bosafo009@st.ug.edu.gh>
2024-06-29 13:09:13 +0000

    completed TicketCaculator

---
+++ assignment1/TicketCalculator/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice;
+
+            if (age <= 12 || age >= 65)
+            {
+                ticketPrice = 7;
+            }
+            else
+            {
+                ticketPrice = 10;
+            }
+
+            Console.WriteLine("The ticket price is GHC" + ticketPrice);
+
+        }
+    }
+}
