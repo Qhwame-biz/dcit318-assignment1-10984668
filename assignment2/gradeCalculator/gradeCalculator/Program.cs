a69d49aaa99e835a0cc9218d6f26a3aaffae45b2
kwame biz <bosafo009@st.ug.edu.gh>
2024-06-29 13:10:13 +0000

    completed grade calculator

---
+++ assignment2/gradeCalculator/gradeCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace gradeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome");
+ Console.WriteLine(" Please Enter your Score from 0 - 100");
+ int Score = Convert.ToInt32(Console.ReadLine());
+ if (Score >= 90)
+ {
+     Console.WriteLine("Your Grade is : A");
+ }
+ else if (Score <= 89 && Score >= 80)
+ {
+     Console.WriteLine("Your Grade is : B");
+
+ }
+ else if (Score <= 79 && Score >= 70)
+ {
+     Console.WriteLine("Your Grade is : C");
+
+ }
+ else if (Score <= 69 && Score >= 60)
+ {
+     Console.WriteLine("Your Grade is : D");
+
+ }
+ else
+ {
+     Console.WriteLine("You Fail");
+ } 
+
+        }
+    }
+}
